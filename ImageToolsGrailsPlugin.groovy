4c3ffeb988331384530b8585234d92b953f50cb1
Ricardo J. Mendez <ricardo@arquetipos.co.cr>
2008-10-27 10:09:06 -0600

    Initial commit for github

---
+++ ImageToolsGrailsPlugin.groovy
@@ -0,0 +1,30 @@
+class ImageToolsGrailsPlugin {
+	def version = "1.0.1";
+	def dependsOn = [:]
+	
+	def author = "Ricardo J. Mendez"
+	def authorEmail = "ricardo@arquetipos.co.cr"
+	def title = "Basic image tools based on JAI"
+	def documentation = 'http://grails.org/ImageTools+plugin'
+	
+	def doWithSpring = {
+		// TODO Implement runtime spring config (optional)
+	}   
+	def doWithApplicationContext = { applicationContext ->
+		// TODO Implement post initialization spring config (optional)		
+	}
+	def doWithWebDescriptor = { xml ->
+		// TODO Implement additions to web.xml (optional)
+	}	                                      
+	def doWithDynamicMethods = { ctx ->
+		// TODO Implement additions to web.xml (optional)
+	}	
+	def onChange = { event ->
+		// TODO Implement code that is executed when this class plugin class is changed  
+		// the event contains: event.application and event.applicationContext objects
+	}                                                                                  
+	def onApplicationChange = { event ->
+		// TODO Implement code that is executed when any class in a GrailsApplication changes
+		// the event contain: event.source, event.application and event.applicationContext objects
+	}
+}
